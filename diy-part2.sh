4bab658e0a8536f66a242cd71e4bdc34cb110c67
YifLee <54547317+YifLee@users.noreply.github.com>
2022-03-05 16:21:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,6 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
+git clone https://github.com/kenzok8/small-package
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
