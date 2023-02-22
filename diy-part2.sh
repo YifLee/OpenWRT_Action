638305e8aa65917e9df00783e4c2bdca4751fc98
YifLee <54547317+YifLee@users.noreply.github.com>
2023-02-22 15:38:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,4 +14,4 @@ git clone https://github.com/kenzok8/small-package package/luci-app-passwall
 git clone https://github.com/kenzok8/small-package package/luci-app-adguardhome
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
